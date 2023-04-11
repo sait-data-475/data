8f6278f39aea4adc81c5ffacfd4d5a961609988a
Zeheng Li <imzehengl@gmail.com>
2023-04-11 00:33:17 -0600

    update script

--- download_data.py
+++ download_data.py
@@ -1,8 +1,12 @@
+import tarfile
 from pathlib import Path
 from urllib.parse import urljoin
 
+import pandas as pd
 import requests
+from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+from tqdm import tqdm
 
 load_dotenv()
 
@@ -36,5 +40,45 @@ with open(output / "Online Retail.xlsx", "wb") as f:
     f.write(r.content)
 
 url = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
+r = requests.get(url)
 with open(output / "aclImdb_v1.tar.gz", "wb") as f:
     f.write(r.content)
+
+
+with tarfile.open(output / "aclImdb_v1.tar.gz") as f:
+    f.extractall(output)
+
+imdb = output / "imdb"
+imdb.mkdir(exist_ok=True)
+
+
+def get_text(path):
+    with open(path, encoding="utf-8") as f:
+        text = f.read()
+
+    text = BeautifulSoup(text, features="html.parser").get_text()
+
+    return text
+
+
+def create_dataset(name):
+    review, sentiment = [], []
+    pos_docs = Path(output / f"aclImdb/{name}/pos")
+    for path in tqdm(list(pos_docs.glob("*.txt")), desc=f"{name} pos"):
+        text = get_text(path)
+        sentiment.append("positive")
+        review.append(text)
+
+    neg_docs = Path(output / f"aclImdb/{name}/neg")
+    for path in tqdm(list(neg_docs.glob("*.txt")), desc=f"{name} neg"):
+        text = get_text(path)
+        sentiment.append("negative")
+        review.append(text)
+
+    pd.DataFrame(dict(review=review, sentiment=sentiment)).to_csv(
+        imdb / f"{name}.csv", index=False
+    )
+
+
+create_dataset("train")
+create_dataset("test")
